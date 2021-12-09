4b62017bc7187838ee9da18eaeef875db77251fd
shakil-babu <personal.shakil.babu@gmail.com>
2021-12-09 06:15:41 +0600

    searching-algorithms

---
+++ 10.searching_algorithms/01.linear_search/01.linear_search_implementation.js
@@ -0,0 +1,12 @@
+// linear search
+// const linearSearch = (arr, target) => {
+//   let found = -1;
+//   for (let i = 0; i <= arr.length; i++) {
+//     if (arr[i] === target) {
+//       found = i;
+//     }
+//   }
+//   return found;
+// };
+// let ans = linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7);
+// console.log(ans);
--- app.js
+++ app.js
@@ -10,33 +10,3 @@
 //   }
 //   return false;
 // }
-
-// binary search
-// const binarySearch = (arr, target) => {
-//   if (arr.length === 0) return -1;
-//   let left = 0;
-//   let right = arr.length - 1;
-//   while (left <= right) {
-//     let mid = Math.floor((left + right) / 2);
-//     if (arr[mid] === target) return mid;
-//     if (arr[mid] < target) {
-//       left = mid + 1;
-//     } else {
-//       right = mid - 1;
-//     }
-//   }
-//   return -1;
-// };
-
-// linear search
-// const linearSearch = (arr, target) => {
-//   let found = -1;
-//   for (let i = 0; i <= arr.length; i++) {
-//     if (arr[i] === target) {
-//       found = i;
-//     }
-//   }
-//   return found;
-// };
-// let ans = linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7);
-// console.log(ans);
