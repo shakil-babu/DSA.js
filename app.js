f004ebcc836a7a005def18d426c2aec8f4ba0c7b
shakil-babu <personal.shakil.babu@gmail.com>
2021-12-10 16:41:58 +0600

    folder-rename

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-// using two pointer
-// function isSubsequence(str1, str2) {
-//   var i = 0;
-//   var j = 0;
-//   if (!str1) return true;
-//   while (j < str2.length) {
-//     if (str2[j] === str1[i]) i++;
-//     if (i === str1.length) return true;
-//     j++;
-//   }
-//   return false;
-// }
+// const say = () => {
+//   console.log("Helo i'm always calling true or false dosen't matter!");
+// };
+
+// let completed = false;
+// let pp = new Promise((resolve, reject) => {
+//   // pending
+//   setTimeout(() => {
+//     if (completed) {
+//       resolve("Hey i've learned about promises."); // resolve
+//     } else {
+//       reject("Sorry, don't learn yet!"); // reject
+//     }
+//   }, 1000);
+// })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     say();
+//   });
+
+// let's discribe about set data-structrues
+let sds = new Set();
+sds.add("a");
+sds.add([1, 2, 3]);
+sds.add({ name: "shakil" });
+console.log(sds);
--- code.js
+++ code.js
@@ -1,24 +1,29 @@
-var hashmap = new Map();
-hashmap.set("name", { name: "shakil" });
-hashmap.set("callme", () => console.log("Hello world!"));
-hashmap.set("favlans", ["js", "c++", "c"]);
-hashmap.set("isMarried", false);
-// hashmap.forEach((item, index) => {
-//   if (index === "isMarried") {
-//     hashmap.set("isMarried", true);
-//   }
-// });
+// var hashmap = new Map();
+// hashmap.set("name", { name: "shakil" });
+// hashmap.set("callme", () => console.log("Hello world!"));
+// hashmap.set("favlans", ["js", "c++", "c"]);
+// hashmap.set("isMarried", false);
+// // hashmap.forEach((item, index) => {
+// //   if (index === "isMarried") {
+// //     hashmap.set("isMarried", true);
+// //   }
+// // });
 
-// console.log(hashmap.get("isMarried"));
+// // console.log(hashmap.get("isMarried"));
 
-if (hashmap.has("isMarried")) {
-  hashmap.set("isMarried", true);
-}
+// if (hashmap.has("isMarried")) {
+//   hashmap.set("isMarried", true);
+// }
 
-let len = hashmap.size();
-console.log(len);
-console.log(hashmap);
+// let len = hashmap.size();
+// console.log(len);
+// console.log(hashmap);
 
-// deleting item from hashmap
-hashmap.delete("isMarried");
-hashmap.clear();
+// // deleting item from hashmap
+// hashmap.delete("isMarried");
+// hashmap.clear();
+
+// function create using buil-in Function
+var body = "return 'Shakil Babu';";
+var say = new Function(body);
+console.log(say());
