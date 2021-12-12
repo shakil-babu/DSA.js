2c6f2a8ee05effe4ff1c989a5ec6df9f7948db37
shakil-babu <personal.shakil.babu@gmail.com>
2021-12-12 15:57:34 +0600

    binary-search-algorithm

--- 10.searching_algorithms/02.binary_search/01.binary_search_implementation.js
+++ 10.searching_algorithms/02.binary_search/01.binary_search_implementation.js
@@ -1,16 +1,28 @@
-// binary search
+// binary search implementation
 const binarySearch = (arr, target) => {
   if (arr.length === 0) return -1;
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) return mid;
-    if (arr[mid] < target) {
-      left = mid + 1;
+  let l = 0,
+    mid;
+  let r = arr.length - 1;
+  while (l <= r) {
+    mid = Math.floor((l + r) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      l = mid + 1;
     } else {
-      right = mid - 1;
+      r = mid - 1;
     }
   }
+
   return -1;
 };
+
+let ans = binarySearch([1, 2, 3, 4, 5, 6, 7, 8], 5);
+console.log(ans);
+
+// Array would be sorted for binary search
+/*
+time complexity : O(log n)
+space complexity : O(1)
+*/
