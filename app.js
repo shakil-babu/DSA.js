3071560e48bfe83df7886daed577e440fcea5cea
shakil-babu <personal.shakil.babu@gmail.com>
2021-12-31 11:16:20 +0600

    dubly-linked-list

--- app.js
+++ app.js
@@ -12,4 +12,18 @@ class DublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let node = new Node(val);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+      node.prev = null;
+    }
+
+    this.length++;
+  }
 }
