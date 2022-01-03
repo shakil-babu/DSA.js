c9d1a1c8af6ee08065f8018e567bced2967672ab
shakil-babu <personal.shakil.babu@gmail.com>
2022-01-03 08:43:06 +0600

    binary=search

--- 10.searching_algorithms/02.binary_search/04.find the floor number from a sorted array that is  smaller than or equal to target.js
+++ 10.searching_algorithms/02.binary_search/04.find the floor number from a sorted array that is  smaller than or equal to target.js
@@ -1,4 +1,4 @@
-// Q : find the ceiling number from a sorted array that is  greator than or equal to target
+// Q : find the floor number from a sorted array that is  smaller than or equal to target
 const ceilingNumber = (arr, target) => {
   let left = 0,
     right = arr.length - 1;
@@ -16,27 +16,27 @@ const ceilingNumber = (arr, target) => {
     }
   }
 
-  return arr[left] || -1;
+  return arr[right] || -1;
 };
 
-let ans = ceilingNumber([1, 2, 3, 4, 7, 8], 5);
+let ans = ceilingNumber([2, 3, 4, 2, 5], 100);
 console.log(ans);
 
 // test cases
 /*
-  arr = [1, 2, 3, 4, 5, 6, 8] , target = 7
-  -> 8
-
-  arr = [10,20,30,40,50], target = 50
-  -> 50
-
-  arr = [1,2,3,4,7,8], target = 5
-  -> 7
-
-  arr = [2,3,4,2,5], target = 100
-  -> -1
-
-
-
-*/
+    arr = [1, 2, 3, 4, 5, 6, 8] , target = 7
+    -> 6
+  
+    arr = [10,20,30,40,50], target = 50
+    -> 50
+  
+    arr = [1,2,3,4,7,8], target = 5
+    -> 4
+  
+    arr = [2,3,4,2,5], target = 100
+    -> 5
+  
+  
+  
+  */
 // [2,3,3,5,6,7,32]
