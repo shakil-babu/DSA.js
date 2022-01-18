bbc78afcafc11c36102ae084ff9a918f82c72a72
shakil-babu <personal.shakil.babu@gmail.com>
2022-01-18 09:15:21 +0600

    Heap

---
+++ 12.Data Structures/21.Heap/Intro.js
@@ -0,0 +1,2 @@
+// https://learnersbucket.com/tutorials/array/heap-data-structure-in-javascript/
+// https://www.educative.io/blog/data-structure-heaps-guide?__cf_chl_managed_tk__=nkNFC0u2auFpa3h_UtfhMYonMq2Sp7G7thCxuTggY2Q-1642474573-0-gaNycGzNC1E
--- code.js
+++ code.js
@@ -1,24 +1,2 @@
-var divideString = function (s, k, fill) {
-  if (s.length === 0) return [];
-  let ans = [];
-  for (let i = 0, knife = 0; i < s.length; i += 1, knife += k) {
-    let slc = s.substr(knife, k);
-    ans.push(slc);
-  }
-
-  let uniqued = ans.filter((item) => item !== "");
-
-  let final = [];
-  uniqued.map((item, index) => {
-    if (item.length < k) {
-      let len = k - item.length;
-      let i = item.concat(fill.repeat(len));
-      final.push(i);
-    } else final.push(item);
-  });
-
-  return final;
-};
-
 let ans = divideString("abcdefghij", 3, "x");
 console.log(ans);
