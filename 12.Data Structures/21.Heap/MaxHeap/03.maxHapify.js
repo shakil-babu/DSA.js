ba63829f2bca28addedf04c6156a4de79e77e7eb
shakil-babu <personal.shakil.babu@gmail.com>
2022-01-20 18:28:02 +0600

    maxhepify

--- 12.Data Structures/21.Heap/MaxHeap/03.maxHapify.js
+++ 12.Data Structures/21.Heap/MaxHeap/03.maxHapify.js
@@ -1,53 +1,27 @@
-class MaxHeap {
-  constructor() {
-    this.values = [];
+// _maxHepify
+function _maxHepify(index = 0) {
+  let left = 2 * index + 1;
+  let right = 2 * index + 2;
+
+  let largest = index;
+  let len = this.values.length;
+
+  // if len is greator than left and left value large than root
+  if (left < len && this.values[largest] < this.values[left]) {
+    largest = left;
   }
 
-  // insert method
-  insert(element) {
-    this.values.push(element);
-    this.bubbleUp();
+  // if len is greator than right and right value large than largest
+  if (right < len && this.values[largest] < this.values[right]) {
+    largest = right;
   }
 
-  // bubbleUp
-  bubbleUp() {
-    let index = this.values.length - 1;
-    let element = this.values[index];
+  if (largest !== index) {
+    let temp = this.values[index];
+    this.values[index] = this.values[largest];
+    this.values[largest] = temp;
 
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parentElement = this.values[parentIndex];
-      if (element <= parentElement) break;
-      this.values[parentIndex] = element;
-      this.values[index] = parentElement;
-      index = parentIndex;
-    }
-  }
-
-  // Hepify
-  _maxHepify(index = 0) {
-    let left = 2 * index + 1;
-    let right = 2 * index + 2;
-    let largest = index;
-
-    let len = this.values.length;
-    if (len > left && this.values[largest] < this.values(left)) {
-      largest = left;
-    }
-
-    if (len > right && this.values[largest] < this.values(right)) {
-      largest = right;
-    }
-
-    // If smallest is not root
-    if (largest !== i) {
-      [this.values[i], this.values[largest]] = [
-        this.values[largest],
-        this.values[i],
-      ];
-
-      // Recursively heapify the affected sub-tree
-      this._maxHepify(largest);
-    }
+    // recursively heapify affected sub-tree
+    this._maxHepify(largest);
   }
 }
